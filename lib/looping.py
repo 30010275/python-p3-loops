d39efe5a1d218136264081ea7b343a2f60e1fdb2
innocent <innocent.wasia@studentmoringaschool.com>
2025-02-13 12:08:33 +0300

    initial commit

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,40 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
+    count = 10
+    while count > 0:
+     print(count)
+    count-= 1
+    print ("Happy New Year!")
+
     # code goes here!
     pass
 
 def square_integers(int_list):
-    # code goes here!
-    pass
+    return[num **2 for num in int_list]
+print(square_integers([1,2,3,4,5]))
+    # code goes here!pass
 
 def fizzbuzz():
-    # code goes here!
+    for num in range(1, 101):  # Loop from 1 to 100
+        if num % 3 == 0 and num % 5 == 0:
+            print("FizzBuzz")
+        elif num % 3 == 0:
+            print("Fizz")
+        elif num % 5 == 0:
+            print("Buzz")
+        else:
+            print(num)
+
+
+
+
+
+
+
+
+
+
+
+
     pass
